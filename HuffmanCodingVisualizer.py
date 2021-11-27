c399c616b37012dcecb049088027901a2056c24c
F. Chavez <77993338+fchv@users.noreply.github.com>
2021-11-27 16:26:54 -0600

    Included Huffman Decoding

--- HuffmanCodingVisualizer.py
+++ HuffmanCodingVisualizer.py
@@ -6,6 +6,7 @@ from pptree import *
 from pptree import Node as treeNode
 
 codes = dict()
+treeRoot = []
 
 class Node:
     def __init__(self, count, symbol, left=None, right=None):
@@ -31,7 +32,8 @@ def get_counts(data):
         if symbols.get(element) == None:
             symbols[element] = 1
         else: 
-            symbols[element] += 1     
+            symbols[element] += 1  
+
     return symbols
 
 def Output_Encoded(data, coding):
@@ -42,7 +44,7 @@ def Output_Encoded(data, coding):
     return string
 
 def space_difference(data, coding):
-    before_compression = len(data) * 8 # total bit space to stor the data before compression
+    before_compression = len(data) * 8 # total bit space to store the data before compression
     after_compression = 0
     symbols = coding.keys()
     for symbol in symbols:
@@ -51,7 +53,10 @@ def space_difference(data, coding):
     print("Space usage before compression (in bits):", before_compression)    
     print("Space usage after compression (in bits):",  after_compression)
 
+
+
 def huffman_encoding():
+
     filename = filedialog.askopenfilename()
     print("Selected: ", filename)
 
@@ -73,7 +78,7 @@ def huffman_encoding():
         nodes.append(Node(symbol_with_counts.get(symbol), symbol))
     
     while len(nodes) > 1:
-        # sort all the nodes in ascending order based on their probability
+        # sort all the nodes in ascending order based on their frequency
         nodes = sorted(nodes, key=lambda x: x.count)
         # for node in nodes:  
         #      print(node.symbol, node.prob)
@@ -87,23 +92,22 @@ def huffman_encoding():
     
         # combine the 2 smallest nodes to create new node
         newNode = Node(left.count+right.count, left.symbol+right.symbol, left, right)
-    
+
         nodes.remove(left)
         nodes.remove(right)
         nodes.append(newNode)
             
-    huffman_encoding = Calculate_Codes(nodes[0])
+    huffman_encoding_dictionary = Calculate_Codes(nodes[0])
     timeB = time.time()
-    print("symbols with codes", huffman_encoding)
-    space_difference(data, huffman_encoding)
-    encoded_output = Output_Encoded(data,huffman_encoding)
+    print("symbols with codes", huffman_encoding_dictionary)
+    space_difference(data, huffman_encoding_dictionary)
+    encoded_output = Output_Encoded(data,huffman_encoding_dictionary)
     print("Time Elapsed (in seconds): ", round(timeB-timeA,3))
-    print(encoded_output)
+    print("Encoded output: " + encoded_output)
     convertToHex(encoded_output)
-    #levelOrderTraversal(nodes[0])
-    
-    
-    #PRINT THE HUFFMAN TREE
+
+
+    ###### PRINT THE HUFFMAN TREE ######
     root = []
     def treeTraversal(node, parent=None):
         nodeContent = str(node.symbol) + ": " + str(node.count) #node will show "character: count"
@@ -121,39 +125,21 @@ def huffman_encoding():
             treeTraversal(node.right, n)
         if node.left != None:
             treeTraversal(node.left, n)
-    treeTraversal(nodes[0])
+
+    treeTraversal(nodes[0]) #Pass the root
+    print('Huffman Tree:')
     print_tree(root[0], horizontal=False)
-    
-    ######PRINT ENCODING RESULTS TO AN OUTPUT FILE######
+
+    ###### PRINT ENCODING RESULTS TO AN OUTPUT FILE ######
     outputFile = open("encodedOutput.txt", "w")
     outputFile.write(encoded_output)
 
-    
+    treeRoot.append( nodes[0] ) #store the tree root in the global variable
     return encoded_output, nodes[0] 
 
-def convertToHex(str):
-    decimalValue = int(str, 2)
-    hexstring = hex(decimalValue)
-    print(hexstring)
 
-
-root = tkinter.Tk()
-
-root.geometry("460x360")
-frame = tkinter.Frame(root)
-frame.grid()
-
-instructions =tkinter.Label(frame, text="Press the button below and select a file for which to"+
-                                        " generate a huffman tree.").grid(row=0, column=1)
-HuffmanEncoding =tkinter.Button(text="run huffman encoding ", command=lambda:[huffman_encoding(), root.destroy()]).grid(row=2, column=0)
-#HuffmanDecoding =tkinter.Button(text="run huffman decoding ", command=lambda:[huffman_decoding(), root.destroy()]).grid(row=3, column=0)
-
-exitButton =tkinter.Button(text="Emergency Exit", command=root.destroy).grid(row=4, column=0)
-
-root.mainloop()
 
 #level order traversal incase its needed
-'''
 def levelOrderTraversal(node):
     def printLevel(node, level):
         if(node is None):
@@ -166,10 +152,69 @@ def levelOrderTraversal(node):
             return True
         lChild = printLevel(node.left, level-1)
         rChild = printLevel(node.right, level-1)
-
         return rChild or lChild
-
     level = 1
     while printLevel(node, level):
         print("\n")
-        level +=1'''
+        level +=1
+
+
+def convertToHex(str):
+    decimalValue = int(str, 2)
+    hexstring = hex(decimalValue)
+    print("Encoded output in hex: " + hexstring)
+
+
+
+#Huffman decoding can ONLY be run after huffman_encoding() has been called and built the huffman tree and encoded file
+def huffman_decoding(root):
+    print("\n\n-------Huffman Decoding-------")
+    filename = filedialog.askopenfilename()
+    print("Selected: ", filename)
+    timeA = time.time()
+
+    with open(filename) as sample:
+        data = sample.read()
+    
+    node = root #set the current node to the root of the tree
+    decoded_output = ""
+    #Go through each bit in the encoded data to navigate through the huffman tree
+    for c in data:
+        if c == '0':
+            #If the left node is a leaf node, add its symbol to the output
+            if (node.left.left == None) and (node.left.right == None):
+                decoded_output += node.left.symbol
+                node = root
+            else:
+                node = node.left
+        else:
+            #If the right node is a leaf node, add its symbol to the output
+            if (node.right.left == None) and (node.right.right == None): 
+                decoded_output += node.right.symbol 
+                node = root
+            else:
+                node = node.right
+        
+    timeB = time.time()
+    print("\nTime Elapsed (in seconds): ", round(timeB-timeA,3))
+    print("Decoded output: " + str(decoded_output))
+    return 
+
+
+root = tkinter.Tk()
+
+root.geometry("460x360")
+frame = tkinter.Frame(root)
+frame.grid()
+
+instructions =tkinter.Label(frame, text="Press the button below and select a file for which to"+
+                                        " generate a huffman tree.").grid(row=0, column=1)
+#HuffmanEncoding =tkinter.Button(text="run huffman encoding ", command=lambda:[huffman_encoding(), root.destroy()]).grid(row=2, column=0)
+#HuffmanDecoding =tkinter.Button(text="run huffman decoding ", command=lambda:[huffman_decoding(), root.destroy()]).grid(row=3, column=0)
+HuffmanEncoding =tkinter.Button(text="run huffman encoding ", command=lambda:[huffman_encoding()]).grid(row=2, column=0)
+HuffmanDecoding =tkinter.Button(text="run huffman decoding ", command=lambda:[huffman_decoding(treeRoot[0])]).grid(row=3, column=0)
+
+
+exitButton =tkinter.Button(text="Emergency Exit", command=root.destroy).grid(row=4, column=0)
+
+root.mainloop()
